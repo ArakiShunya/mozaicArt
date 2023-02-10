96c217a28f2f9d0468ea5b8b254cb95128b0609e
ArakiShunya <123075060+ArakiShunya@users.noreply.github.com>
2023-02-10 18:36:23 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 function makeMosaic(){
 	const subImage_origin = document.getElementById('subImage').files[0];
-	const testImage = document.getElementById('testImage').files[0];
+	const testImage = document.getElementById('testImage');
 	test(subImage_origin);
 	const subImage = resizeImage(subImage_origin);
 	console.log(subImage);
