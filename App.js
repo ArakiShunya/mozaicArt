e304bbd7515617a7ca54736f01a90f7e61743bf7
ArakiShunya <123075060+ArakiShunya@users.noreply.github.com>
2023-02-12 19:48:38 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 function makeMosaic(){
 	const subImage_origin = document.getElementById('subImage').files[0];
 	const subImage = resizeImage(subImage_origin);
+	console.log(subImage);
 	const materialImage_origin = document.getElementById("materialImage").files;
 	console.log(materialImage_origin);
 	const materialImagesColors = resizeGetColor(materialImage_origin);
