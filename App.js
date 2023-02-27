4b597c72edaa785796ae65d1f45295b3a4655c5a
ArakiShunya <123075060+ArakiShunya@users.noreply.github.com>
2023-02-27 21:39:29 +0900

    Update App.js

--- App.js
+++ App.js
@@ -82,7 +82,7 @@ async function getMosaicImage(subImage, images, colors){
 			console.log("min : " + min);
 			near = d.indexOf(min);
 			console.log(near);
-			ct.drawImage(images[near], x * 300, y * 200, (x + 1) * 300, (y + 1) * 200);
+			ct.putImageData(images[near], x * 300, y * 200, (x + 1) * 300, (y + 1) * 200);
 		}
 	}
 }
